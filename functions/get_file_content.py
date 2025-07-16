b432165a745cb815cb71337d86a4c9f3fc3df234
PicusB <picusbrit@gmail.com>
2025-07-16 18:19:10 -0400

    added file content reading

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,9 +1,28 @@
-from config import MAX_CHARS
+from functions import config
 import os
 
 
 def get_file_content(working_directory, file_path):
-    absolute_path_to_wd = os.path.abspath(working_directory)
-    absolute_path_to_file = os.path.abspath(os.path.join(working_directory, file_path))
+    try:
+        absolute_path_to_wd = os.path.abspath(working_directory)
+        absolute_path_to_file = os.path.abspath(os.path.join(working_directory, file_path))
+    except:
+        return f'Error: Issue creating absolute path to file, check to make sure the input is valid'
     if not absolute_path_to_file.startswith(absolute_path_to_wd):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directroy'
+    if not os.path.isfile(absolute_path_to_file):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(absolute_path_to_file, "r") as f:
+            file_content_string = f.read(config.MAX_CHARS+1)
+            if len(file_content_string)>config.MAX_CHARS:
+                file_content_string = file_content_string[:-1]
+                file_content_string+=f'[...File "{file_path} truncated at {config.MAX_CHARS} characters]'
+                return file_content_string
+            else:
+                return file_content_string
+    except:
+        return f'Error: Error reading the file, ensure the file is valid and the correct type'
+    
+    
+    
--- tests.py
+++ tests.py
@@ -1,15 +1,16 @@
-from functions.get_files_info import get_files_info
+from functions import get_files_info
+from functions.get_file_content import get_file_content
 
-tests = [["calculator", "."],
-         ["calculator", "pkg"],
-         ["calculator", "/bin"],
-         ["calculator", "../"]
+tests = [["calculator", "main.py"],
+         ["calculator", "pkg/calculator.py"],
+         ["calculator", "/bin/cat"],
+         ["calculator", "pkg/doesn_not_exist.py"]
          ]
 
 
 def run_test(tests):
     for test in tests:
-        result = get_files_info(test[0], test[1])
+        result = get_file_content(test[0], test[1])
         print(result)
 
 def main():
